19be9ee0fa4b440a79e65792c9e498ffee33a059
Mamoudou DIALLO <mamoudoudiallo2397@gmail.com>
2024-10-23 09:31:20 +0200

    Finished the crud

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use HttpResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,7 +24,7 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/add', name: 'add_product', methods: ['POST'])]
+    #[Route('/product', name: 'add_product', methods: ['POST'])]
     public function add(
         #[MapRequestPayload]
         Product $product, EntityManagerInterface $manager): JsonResponse
@@ -32,6 +33,65 @@ class HomeController extends AbstractController
         $manager->flush();
 
         return $this->json([$product]);
+    }
+
+    #[Route('/products', name: 'get_products', methods: ['GET'])]
+    public function getProducts(ProductRepository $productRepository): JsonResponse
+    {
+        return $this->json($productRepository->findAll());
+    }
+
+    #[Route('/products/{id}', name: 'get_product', methods: ['GET'])]
+    public function getProduct(Request $request, ProductRepository $productRepository): JsonResponse
+    {
+        $product = $productRepository->find($request->get('id'));
+        if($product === null) {
+            return $this->json([
+                "message" => "Product not found"
+            ],
+                404
+            );
+        }
+        return $this->json($product);
+    }
+
+    #[Route('/products/delete/{id}', name: 'delete_product', methods: ['DELETE'])]
+    public function deleteProduct(Request $request, ProductRepository $productRepository, EntityManagerInterface
+    $manager):JsonResponse
+    {
+        $product = $productRepository->find($request->get('id'));
+        if($product === null) {
+            return $this->json([
+                "message" => "Product not found"
+            ],
+                404
+            );
+        }
+        $manager->remove($product);
+        $manager->flush();
+
+        return $this->json([
+            "message" => "The product was successfully deleted!",
+            'product' => $product
+        ]);
+    }
+
+    #[Route('/product/update', name: 'update_product', methods: ['PUT'])]
+    public function updateProduct(ProductRepository $productRepository, EntityManagerInterface $manager, Request $request)
+    :JsonResponse
+    {
+        $newProduct = json_decode($request->getContent(), true);
+        $product = $productRepository->findOneBy(["id"=>$newProduct["id"]]);
+
+        $product->setName($newProduct["name"]);
+        $product->setPrice($newProduct["price"]);
+        $product->setDescription($newProduct["description"]);
+        $product->setUpdatedAt(new \DateTimeImmutable());
+
+        $manager->persist($product);
+        $manager->flush();
+
+        return $this->json($product);
     }
 
 
